8bc8223875102735043ab24bfb25b76fc055751f
Gustavo Juan <gustavojuandev@gmail.com>
2023-10-26 12:27:14 +0200

    [FEAT] Funcionalidades Counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'Hola mundo';
-  public counter: number = 10;
 
-  increaseBy(value: number): void {
-    this.counter += value;
-  }
-  resetCounter():void{
-    this.counter =10
-  }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola soy counter</h1>',
+  templateUrl: 'counter.component.html'
 })
 
 export class CounterComponent{
+
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+  resetCounter(): void {
+    this.counter = 10
+  }
+
+
 
 }
