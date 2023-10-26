2d586d0c49634263bd14eaef0e7c88df6b6366c1
Gustavo Juan <gustavojuandev@gmail.com>
2023-10-26 12:49:50 +0200

    [FEAT] Interpolación, estructura HTML y estilos

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {
 
+
+export class HeroComponent {
+  public name:string = 'ironman';
+  public age:number = 45;
 }
